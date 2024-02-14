83dd230c01b02fbdf0b750f26051bd585c32e498
Akeem1955 <adetunjiadams29@gmail.com>
2024-02-14 13:59:51 +0100

    All Complained Bug Solved-- progress 99% completed

--- app/src/main/java/com/pioneers/jobgig/dataobj/utils/Utility.kt
+++ app/src/main/java/com/pioneers/jobgig/dataobj/utils/Utility.kt
@@ -1,13 +1,11 @@
 package com.pioneers.jobgig.dataobj.utils
 
-import android.net.Uri
 import com.pioneers.jobgig.screens.DonateType
 
 
 data class InstructorDesignData(var uri: String ="", var name:String="", var description:String="")
 data class Category(var color:String="", var icon:String="", var userEnrolled:Int=0)
 data class  CategoryItems(var categories:List<Category> = emptyList())
-data class DonationQueue(var donationQueue:List<DonationQueue> = emptyList())
 data class DonationRequest(var reason:String="",
                            var target:Double = 0.0,
                            var raised:Double = 0.0,
